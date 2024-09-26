785b563939357302f7eadf8da232bfcac82b973e
CaiozinS2 <caioandersongoes@gmail.com>
2024-09-25 22:48:06 -0300

    Criação de Salas com Socket.io e armazenamento de dados localmente

--- src/socket-back.js
+++ src/socket-back.js
@@ -1,10 +1,40 @@
 import io from "./servidor.js";
 
+const documentos = [
+    {
+        nome: 'JavaScript',
+        texto: 'texto de JavaScript'
+    },
+    {
+        nome: 'Node',
+        texto: 'texto de Node.js'
+    },
+    {
+        nome: 'Socket-io',
+        texto: 'texto de Socket-io'
+    },
+]
+
 io.on("connection", (socket) => {
     console.log("Um cliente se conectou! ID:", socket.id);
 
-    socket.on("texto_editor", (texto) => {
-        socket.broadcast.emit("texto_editor_clientes", texto)
-    })
-})
+    socket.on("selecionar_documento", (nomeDocumento) => {
+        const documento = encontrarDocumento(nomeDocumento);
 
+        console.log(documento)
+
+        socket.join(nomeDocumento)
+    });
+
+    socket.on("texto_editor", ({ texto, nomeDocumento }) => {
+        socket.to(nomeDocumento).emit("texto_editor_clientes", texto)
+    });
+});
+
+function encontrarDocumento(nome) {
+    const documento = documentos.find((documento) => {
+        return documento.nome === nome
+    })
+
+    return documento;
+}
