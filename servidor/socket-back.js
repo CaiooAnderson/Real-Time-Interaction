f68e592a1a443f3be79f0eb0691a521dba132caf
CaiozinS2 <caioandersongoes@gmail.com>
2024-10-11 11:16:37 -0300

    Adicionando locação de usuário logado para a tela de documentos

--- servidor/socket-back.js
+++ servidor/socket-back.js
@@ -1,6 +1,7 @@
 import registrarEventosDocumento from "./registrarEventos/documento.js";
 import registrarEventosInicio from "./registrarEventos/inicio.js";
 import registrarEventosCadastro from "./registrarEventos/cadastro.js";
+import registrarEventosLogin from "./registrarEventos/login.js";
 
 import io from "./servidor.js";
 
@@ -8,4 +9,5 @@ io.on("connection", (socket) => {
   registrarEventosInicio(socket, io);
   registrarEventosDocumento(socket, io);
   registrarEventosCadastro(socket, io);
+  registrarEventosLogin(socket, io);
 });
