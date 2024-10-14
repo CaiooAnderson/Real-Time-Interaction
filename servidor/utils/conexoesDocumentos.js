31a865a1288bf7b08a04381e07ca03482f277853
CaiozinS2 <caioandersongoes@gmail.com>
2024-10-14 00:02:04 -0300

    Guardando informações no Socket

--- servidor/utils/conexoesDocumentos.js
+++ servidor/utils/conexoesDocumentos.js
@@ -1,5 +1,11 @@
 const conexoesDocumentos = [];
 
+function encontrarConexao(nomeDocumento, nomeUsuario) {
+  return conexoesDocumentos.find((conexao) => {
+    return ( conexao.nomeDocumento === nomeDocumento && conexao.nomeUsuario === nomeUsuario )
+  })
+}
+
 function adicionarConexao(conexao) {
   conexoesDocumentos.push(conexao);
 }
@@ -10,4 +16,16 @@ function obterUsuariosDocumento(nomeDocumento) {
     .map((conexao) => conexao.nomeUsuario);
 }
 
-export { adicionarConexao, obterUsuariosDocumento };
+function removerConexao(nomeDocumento, nomeUsuario) {
+  const indice = conexoesDocumentos.findIndex((conexao) => {
+    return ( conexao.nomeDocumento === nomeDocumento && conexao.nomeUsuario === nomeUsuario )
+  })
+
+  if (indice !== -1) {
+    conexoesDocumentos.splice(indice, 1)
+  }
+
+  console.log(conexoesDocumentos)
+}
+
+export { encontrarConexao, adicionarConexao, obterUsuariosDocumento, removerConexao };
